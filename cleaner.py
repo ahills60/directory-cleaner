4c558ac80c5ec27c2c9bd39b9dd7d5a233b82f51
Andrew Hills <a.hills@sheffield.ac.uk>
2019-11-12 09:58:07 +0000

    Add check for cleaned paths
    - Paths are checked for a previous run of the cleaner function.
    - Forced mode enables the application to run over the entire directory tree.

--- cleaner.py
+++ cleaner.py
@@ -4,6 +4,8 @@ import filecmp
 import os
 import shutil
 import argparse
+import pathlib
+import sys
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Only keep unique subdirectories")
@@ -13,10 +15,13 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--simulate", help="Simulation mode on", action="store_true")
     group.add_argument("-m", "--modifytime", help="Sort by modify time", action="store_true")
     group.add_argument("-c", "--creationtime", help="Sort by creation time", action="store_true")
+    parser.add_argument("-f", "--force", help="Force execution", action="store_true")
 
     args = parser.parse_args()
     verbose = args.verbose
     simulate = args.simulate
+    forced = args.force
+
     if verbose:
         print("Verbose mode on")
     if args.path:
@@ -72,12 +77,31 @@ if __name__ == "__main__":
         if verbose:
             print("Sorting subdirectories by creation time...")
         dirList.sort(key = lambda s: os.path.getctime(s))
-
     else:
         dirList.sort()
 
+    # Quickly determine what has been scanned before, based on marker
+    startFrom = None
+    for idx, item in enumerate(dirList):
+        if not os.path.isfile(os.path.join(item, ".cleaned")):
+            print("Here {}".format(idx))
+            if idx > 0:
+                startFrom = idx - 1
+            break
+    
+    if startFrom:
+        dirList = dirList[startFrom:]
+    else:
+        # Nothing to do. Continue regardless?
+        if not forced:
+            print("Nothing to do. Termininating.")
+            sys.exit(0)
+
     # Take the first item within the list
     item1 = dirList.pop(0)
+
+    # Always clean item1. This may have been created before, but touch() is okay with that
+    pathlib.Path(os.path.join(item1, ".cleaned")).touch()
 
     # For every item within the directory list
     while dirList:
@@ -88,6 +112,9 @@ if __name__ == "__main__":
             # They're different
             if verbose:
                 print('Keeping unique directory "{}"'.format(item2))
+            # Create an empty file to denote a cleaned directory
+            if not simulate:
+                pathlib.Path(os.path.join(item2, ".cleaned")).touch()
             item1 = item2
         else:
             #  They're the same, so delete item2 in favour of item1
